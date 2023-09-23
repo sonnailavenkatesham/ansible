58fe091ffb3632110b2f42b5761b0d723db6470a
Roman Reings <Rajkumar>
2023-09-23 13:13:47 +0530

    add files

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,8 +11,9 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
     echo " Name:$i and IP:$IP_ADDRESS "
-    aws route53 change-resource-record-sets --hosted-zone-id Z0997824248HW2XYA9N5U --change-batch {
-            "Comment": "CREATEa record ",
+    aws route53 change-resource-record-sets --hosted-zone-id Z0997824248HW2XYA9N5U --change-batch
+    {
+            "Comment": "CREATE/DELETE/UPSERT a record ",
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
@@ -20,7 +21,6 @@ do
                                     "Type": "A",
                                     "TTL": 1,
                                  "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
-                            }
-                        }]
-}
+    }}]
+    }
 done
