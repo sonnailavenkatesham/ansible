931069637fe28604e21008943b2d8ba36166bb9c
Roman Reings <Rajkumar>
2023-09-23 11:30:37 +0530

    add files

---
+++ create-ec2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+IMAGE_ID=ami-03265a0778a880afb
+INSTANCE_TYPE=t2.micro
+SECURITY_GROUP=sg-0f8c523d0f4ed489b
+HOTSTED_ZONE=Z0997824248HW2XYA9N5U
+DOMAIN_NAME=venkateshamsonnalia143.online
+NAME=$@
+for i in $@
+do 
+    echo " Name $num: $i "
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+
+    echo " Name $i and $IP_ADDRESS "
+    aws route53 change-resource-record-sets --hosted-zone-id Z0997824248HW2XYA9N5U --change-batch 
+    '{
+            "Changes": [{
+            "Action": "CREATE a record",
+                        "ResourceRecordSet": {
+                                "Name": "'$i.$DOMAIN_NAME'",
+                                "Type": "A",
+                                "TTL": 1,
+                                "ResourceRecords": [{"'Value": "$IP_ADDRESS'"}]
+                        }
+                }]
+    }'
+done
